764f5f8e9c284b79fb80100a90facefe399b9436
Zach White <zechnwhite@gmail.com>
2019-11-20 00:43:09 -0800

    updates mongodb doc

--- routes/api/rockscores.js
+++ routes/api/rockscores.js
@@ -5,12 +5,25 @@ const router = express.Router();
 const Rockscore = require('../../models/Rockscore');
 
 router.get('/', (req, res) => {
+  // Rockscore.findOneAndUpdate({ username: 'pedroro' }, { $set: { score: 50 } }, { new: true }, function(err, doc) {
+  // });
+
+  // ^^^ABOVE IS HOW TO SUCCESSFULLY UPDATE A DOC IN MONGODB USING MONGOOSE!!!!
+
   Rockscore.find()
-    .sort({ score: -1 })
-    .then(rockscores => res.json(rockscores));
+  .sort({ score: -1 })
+  .then(rockscores => res.json(rockscores));
 });
 
+
+
 router.post('/', (req, res) => {
+  const filter = { username: 'pedroro' };
+  const update = { score: 59 };
+  
+  Rockscore.findOneAndUpdate(filter, update);
+  
+
   const newRockscore = new Rockscore({
     username: req.body.name,
     score: req.body.score
