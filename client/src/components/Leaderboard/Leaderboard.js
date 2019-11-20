1711cc8f9b7c50a5b903c17ca3af652d41a71c40
Zach White <zechnwhite@gmail.com>
2019-11-19 22:06:25 -0800

    more spots on leaderboard

--- client/src/components/Leaderboard/Leaderboard.js
+++ client/src/components/Leaderboard/Leaderboard.js
@@ -25,6 +25,11 @@ function Leaderboard(props) {
     <li>3. {scores[2].username} ||{scores[2].score}%||</li>
     <li>4. {scores[3].username} ||{scores[3].score}%||</li>
     <li>5. {scores[4].username} ||{scores[4].score}%||</li>
+    <li>6. {scores[5].username} ||{scores[5].score}%||</li>
+    <li>7. {scores[6].username} ||{scores[6].score}%||</li>
+    <li>8. {scores[7].username} ||{scores[7].score}%||</li>
+    <li>9. {scores[8].username} ||{scores[8].score}%||</li>
+    <li>10. {scores[9].username} ||{scores[9].score}%||</li>
 
       </div>
     )} else {
