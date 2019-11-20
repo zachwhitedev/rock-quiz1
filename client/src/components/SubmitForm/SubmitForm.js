f0ceefb069efe7d557c2180df5949023bfd082f7
Zach White <zechnwhite@gmail.com>
2019-11-19 19:26:33 -0800

    some styling on submit form

--- client/src/components/SubmitForm/SubmitForm.js
+++ client/src/components/SubmitForm/SubmitForm.js
@@ -3,53 +3,46 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 import styles from './SubmitForm.module.css';
 
-function SubmitForm (props){
+function SubmitForm(props) {
+  const [username, setUsername] = useState('');
 
-    const[username, setUsername] = useState('');
+  const handleChange = e => {
+    setUsername(e.target.value);
+  };
 
-    const handleChange = e => {
-      setUsername(e.target.value);
+  const sendScoreToDB = () => {
+    const newScore = {
+      name: username,
+      score: props.score
     };
-    
-    const sendScoreToDB = () => {
-      const newScore = {
-        name: username,
-        score: props.score
-      };
-      axios
-        .post('/api/rockscores', newScore)
-        .then(res => res.data)
-        .catch(err => console.loge(err));
-    };
-    
-    
-      return (
-        <form className={styles.submitform}>
-          <div>
-            <input
-              type='text'
-              id='username'
-              name='username'
-              placeholder='username'
-              onChange={handleChange}
-              value={username}
-              required
-            />
-          </div>
-          <button onClick={sendScoreToDB}>Submit!</button>
-        </form>
-      );
+    axios
+      .post('/api/rockscores', newScore)
+      .then(res => res.data)
+      .catch(err => console.loge(err));
+  };
 
-}    
+  return (
+    <div className={styles.submitform}>
+        <input
+          type='text'
+          id='username'
+          name='username'
+          placeholder='username'
+          onChange={handleChange}
+          value={username}
+          required
+        />
+        <button onClick={sendScoreToDB}>Submit!</button>
+    </div>
+  );
+}
 
 function mapStoreToProps(store) {
-    return {
-        score: store.quiz.score,
-        questionslength: store.quiz.questionslength,
-        currentQuestion: store.quiz.currentQuestion,
-        isFinished: store.quiz.isFinished
-    };
+  return {
+    score: store.quiz.score,
+    questionslength: store.quiz.questionslength,
+    currentQuestion: store.quiz.currentQuestion,
+    isFinished: store.quiz.isFinished
+  };
 }
 export default connect(mapStoreToProps)(SubmitForm);
-
-
