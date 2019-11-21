eb0d6ce769364de414541b77fbfa29a5d45941da
Zach White <zechnwhite@gmail.com>
2019-11-20 17:20:08 -0800

    adds question

--- client/src/questions.js
+++ client/src/questions.js
@@ -2,39 +2,48 @@
 const questions = [
     {
       id: 1,
-      question: "What band has the album 'Dark Side of the Moon'?",
-      answer_a: "Led Zeppelin",
-      answer_b: "Pink Floyd",
-      answer_c: "Rolling Stones",
-      answer_d: "The Beatles",
-      correct_answer: "b"
-    },
-    {
-      id: 2,
-      question: "What band has the song 'Paint It Black'?",
-      answer_a: "Led Zeppelin",
-      answer_b: "Pink Floyd",
-      answer_c: "Rolling Stones",
+      question: "David Gilmour was a member of which legendary rock group?",
+      answer_a: "Rolling Stones",
+      answer_b: "Led Zeppelin",
+      answer_c: "Pink Floyd",
       answer_d: "The Beatles",
       correct_answer: "c"
     },
     {
+      id: 2,
+      question: "Who wrote the song 'All Along the Watchtower'?",
+      answer_a: "Jimmy Paige",
+      answer_b: "Jimi Hendrix",
+      answer_c: "Neil Young",
+      answer_d: "Bob Dylan",
+      correct_answer: "d"
+    },
+    {
       id: 3,
-      question: "What band has the album 'OK Computer'?",
+      question: "What band made the album 'OK Computer'?",
       answer_a: "Radiohead",
-      answer_b: "Pink Floyd",
-      answer_c: "Rolling Stones",
-      answer_d: "The Beatles",
+      answer_b: "Nirvana",
+      answer_c: "Talking Heads",
+      answer_d: "The Police",
       correct_answer: "a"
     },
     {
       id: 4,
-      question: "What band wrote the song 'Here Comes the Sun?",
-      answer_a: "The Faces",
-      answer_b: "The Rolling Stones",
-      answer_c: "Led Zeppelin",
-      answer_d: "The Beatles",
+      question: "The groovy Beatles song 'Octopus's Garden' is sung by which Beatle?",
+      answer_a: "Paul McCartney",
+      answer_b: "John Lennon",
+      answer_c: "George Harrison",
+      answer_d: "Ringo Starr",
       correct_answer: "d"
+,    },
+    {
+      id: 5,
+      question: "Who produced the famous album 'Pet Sounds'?",
+      answer_a: "Quincy Jones",
+      answer_b: "Brian Wilson",
+      answer_c: "T-Bone Burnett",
+      answer_d: "Dre. Dre",
+      correct_answer: "b"
     }
   ];
 
